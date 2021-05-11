0ad825047df17b3c4848ab6acf13f9ceb3a52428
Monali <khandelwal.monali@gmail.com>
2021-05-11 13:27:08 -0400

    Fixed navigation bar

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import {Button, Form, FormControl, Nav, Navbar} from "react-bootstrap";
+import {Button, Container, Form, FormControl, Nav, Navbar} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Navigation.css';
 
 const Navigation = () => {
     return (
         <>
-            <Navbar className="nav-bar-container" bg="light" variant="light" fixed="top">
+            <Navbar className="nav-bar-container" bg="light" variant="light" sticky="top">
+                <Container>
                 <Navbar.Brand href="#home">Hephaestus</Navbar.Brand>
                 <Nav className="mr-auto">
                     <Nav.Link href="/posts">Home </Nav.Link>
@@ -16,6 +17,7 @@ const Navigation = () => {
                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
                     <Button variant="outline-primary">Search</Button>
                 </Form>
+                </Container>
             </Navbar>
         </>
     );
