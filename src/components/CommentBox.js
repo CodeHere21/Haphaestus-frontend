f83da8006998a19d6fc8ddfe8f002eecf34c2186
Monali <khandelwal.monali@gmail.com>
2021-05-16 20:48:39 -0400

    commit

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,67 +1,50 @@
 import {Form} from "react-bootstrap";
 import axios from 'axios'
-import React from 'react';
+import React, {useState} from 'react';
 import Button from "react-bootstrap/Button";
 
 
-class CommentBox extends React.Component {
+function CommentBox (props){
+    const [author, setAuthor] = useState(),
+        [body, setBody] = useState(),
+        [r, setR ] = useState(),
+        authorHandler = e => setAuthor(e.target.value),
+        bodyHandler = e => setBody(e.target.value)
 
-
-    constructor(props) {
-        super(props);
-        this.state = {comments: [], errorMessage:"" };
-        this.handleAuthorChange = this.handleChange.bind(this, 'author');
-        this.handleCommentChange = this.handleChange.bind(this, 'comment');
-    }
-
-    handleChange(keyName, event) {
-        this.setState({[keyName]: event.target.value});
-    }
-
-    componentDidMount() {
-        const address='https://hephaestus-backendv1.herokuapp.com/posts/'+this.props.value;
-        axios.get(address)
+    const onFormSubmit = e => {
+        e.preventDefault();
+        const num=parseInt(props.toPost, 10)
+        const myDate = new Date().toISOString().slice(0, 10).replace('T', ' ');
+        const postInfo = {body: body, author: author,  writtenOn: myDate, postId:num};
+        axios.post('https://hephaestus-backendv1.herokuapp.com/comments/', postInfo)
             .then(response => {
                 console.log(response)
-                this.setState({comments:response.data})
+                setR(response.data)
             })
-            .catch(error=>{
-                console.log(error)
-                this.setState({errorMessage:"We got a problem"})
-            })
+            .catch(err=>{console.log(err)})
+
     }
 
-    handleSubmit(event) {
-        const commentInfo = {author: this.state.author, comment: this.comment.body};
-        axios.post('https://hephaestus-backendv1.herokuapp.com/posts/{id}', commentInfo)
-            .then(response => this.setState({
-                comment: response.data.comment,
-                author: response.data.author
-            }));
-    }
+    return (
+        <Form className="form-inline" onSubmit={onFormSubmit}>
+            <Form.Group controlId="formBasicName">
+                <Form.Label>Enter Name</Form.Label>
+                <Form.Control type="text"  onChange={authorHandler} placeholder="Name goes here" />
+                <Form.Text>What do you want us to call you?</Form.Text>
+            </Form.Group>
 
-    render (){
-        return (
+            <Form.Group controlId="formBasicComment">
+                <Form.Label>Enter Comment</Form.Label>
+                <Form.Control type="text" onChange={bodyHandler} placeholder="Text goes here" />
+                <Form.Text>What do you wanna say?</Form.Text>
 
-            <Form className="form-inline" onSubmit={this.handleSubmit}>
-                <Form.Group controlId="formBasicName">
-                    <Form.Label>Comments</Form.Label>
-                    <Form.Control type="userName" placeholder="Enter name" onChange={this.handleAuthorChange}/>
-                </Form.Group>
-
-                <Form.Group controlId="formBasicComment">
-                    <br/>
-                    <Form.Control type="comment" placeholder="Please say something..." onChange={this.handleCommentChange}/>
-                </Form.Group>
-
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
-        )
-    }
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+    )
 
 }
+
 export default CommentBox;
-
-
